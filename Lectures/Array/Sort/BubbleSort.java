22e80d3d03aa3cdb9e91b56ceef152ff2e7ea143
Dharmik Shinde <dharmikvs26@gmail.com>
2024-02-28 23:15:43 +0530

    Bubble Sort

--- Lectures/Array/Sort/BubbleSort.java
+++ Lectures/Array/Sort/BubbleSort.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -25,17 +26,25 @@ public class BubbleSort {
         // To close Scanner object to prevent resource leak
         in.close();
     }
-
+    
+    // Method to perform bubble sort and return the sorted array
     static int[] bubbleSort(int[] arr) {
         int n = arr.length;
+        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
             }
         }
         return arr;
